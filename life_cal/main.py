b5443f9c2b7349805c3583b6f3c580ae1d597d2e
Vinay Hiremath <mail@vinayh.com>
2022-01-19 15:04:39 +0000

    Cleaned up old Nix files

--- life_cal/main.py
+++ life_cal/main.py
@@ -11,3 +11,6 @@ async def root():
 def start():
     """Launched with `poetry run start` at root level"""
     uvicorn.run("life_cal.main:app", host="0.0.0.0", port=8000, reload=True)
+
+if __name__ == "__main__":
+    start()
